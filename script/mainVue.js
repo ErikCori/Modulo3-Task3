a850993991443829389fb8855b99b4a465ec3af6
Erik Cori <erik199666@hotmail.com>
2019-07-01 12:16:21 -0300

    cambios git commit -m cambios

--- script/mainVue.js
+++ script/mainVue.js
@@ -2,9 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         section: "home",
+				partidos : [],
     }
 })
 
+app.partidos = data.partidos;
+	
 function cambiarSection(nuevaSeccion){
   app.section = nuevaSeccion;
 }
